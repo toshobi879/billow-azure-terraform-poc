7589c1fca74ae9858d68e2fdf06d471471ec32e4
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-02-25 17:58:08 +0530

    Update backend.tf

--- Terraform/vpc/backend.tf
+++ Terraform/vpc/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstateprodstorage123"   # globally unique
+    storage_account_name = "tfstateprodstorage123"  
     container_name       = "tfstate"
     key                  = "network/${var.environment}.tfstate"
   }
+
 }
