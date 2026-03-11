899d05ab18f6adab4487b28cc63b809d4b5fdfaf
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-11 21:47:21 +0530

    Update main.tf

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -1,10 +1,8 @@
-data "azurerm_client_config" "current" {}
+############################################
+# Storage Account for Terraform State
+############################################
 
-########################################
-# Storage Account (Terraform State)
-########################################
-
-resource "azurerm_storage_account" "terraform_state" {
+resource "azurerm_storage_account" "tf_state" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
@@ -15,9 +13,14 @@ resource "azurerm_storage_account" "terraform_state" {
   allow_nested_items_to_be_public = false
 
   blob_properties {
+
     versioning_enabled = true
 
     delete_retention_policy {
+      days = 7
+    }
+
+    container_delete_retention_policy {
       days = 7
     }
   }
@@ -26,27 +29,25 @@ resource "azurerm_storage_account" "terraform_state" {
     prevent_destroy = true
   }
 
-  tags = {
-    purpose = "terraform-backend"
-  }
+  tags = var.tags
 }
 
-########################################
-# Blob Container (State Container)
-########################################
+############################################
+# Blob Container
+############################################
 
-resource "azurerm_storage_container" "terraform_state" {
+resource "azurerm_storage_container" "tf_state" {
   name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.terraform_state.name
+  storage_account_name  = azurerm_storage_account.tf_state.name
   container_access_type = "private"
 }
 
-########################################
+############################################
 # RBAC for GitHub OIDC
-########################################
+############################################
 
-resource "azurerm_role_assignment" "terraform_backend_access" {
-  scope                = azurerm_storage_account.terraform_state.id
+resource "azurerm_role_assignment" "github_backend_access" {
+  scope                = azurerm_storage_account.tf_state.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = var.github_principal_id
 }
