972749ee848f9a8fd4b8d7a2ad0d08e39452a205
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-11 21:48:01 +0530

    Update outputs.tf

--- terraform/bootstrap/outputs.tf
+++ terraform/bootstrap/outputs.tf
@@ -1,7 +1,11 @@
 output "storage_account_name" {
-  value = azurerm_storage_account.terraform_state.name
+  value = azurerm_storage_account.tf_state.name
 }
 
 output "container_name" {
-  value = azurerm_storage_container.terraform_state.name
+  value = azurerm_storage_container.tf_state.name
+}
+
+output "storage_account_id" {
+  value = azurerm_storage_account.tf_state.id
 }
