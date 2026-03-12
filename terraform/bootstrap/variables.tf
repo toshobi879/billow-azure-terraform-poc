ef53ab9f7a802a075154276363eafe459123b434
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-13 01:15:45 +0530

    Update variables.tf

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -1,29 +1,14 @@
 variable "resource_group_name" {
-  description = "Terraform backend resource group"
-  type        = string
-}
-
-variable "location" {
-  description = "Azure region"
+  description = "Existing resource group for Terraform backend"
   type        = string
 }
 
 variable "storage_account_name" {
-  description = "Terraform backend storage account"
+  description = "Terraform state storage account name"
   type        = string
 }
 
 variable "container_name" {
-  description = "Terraform state container"
+  description = "Terraform state container name"
   type        = string
-}
-
-variable "tags" {
-  description = "Resource tags"
-  type        = map(string)
-
-  default = {
-    managed-by  = "terraform"
-    environment = "backend"
-  }
 }
