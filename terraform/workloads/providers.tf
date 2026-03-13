0c41eda8754ac690a380198f7cac7ce33dc560a6
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-14 01:34:01 +0530

    Update providers.tf

--- terraform/workloads/providers.tf
+++ terraform/workloads/providers.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 
-  skip_provider_registration = true
+  use_oidc = true
 }
