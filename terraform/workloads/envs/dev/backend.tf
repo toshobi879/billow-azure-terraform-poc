9ddb5ce1f950f4a319562ae62a6cc64fd930ccf8
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-14 01:13:28 +0530

    Update backend.tf

--- terraform/workloads/envs/dev/backend.tf
+++ terraform/workloads/envs/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Terraform_IaC_POC"
-    storage_account_name = "tfstatedevpoc123"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    storage_account_name = "tfstatebillowpoc123"
+    container_name       = "tfstatepoc"
+    key                  = "network-dev.tfstate"
   }
 }
