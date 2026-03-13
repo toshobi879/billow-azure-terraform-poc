7ab25164f7b74ab49de6d21809c611e70de8422d
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-14 01:03:18 +0530

    Create variables.tf

---
+++ terraform/workloads/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {}
+variable "location" {}
+variable "project_name" {}
+
+variable "vnet_cidr" {}
+variable "aks_subnet_cidr" {}
+variable "db_subnet_cidr" {}
+variable "appgw_subnet_cidr" {}
+variable "private_endpoint_subnet_cidr" {}
