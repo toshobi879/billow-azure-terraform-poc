706a5c0bfdfec4186e1b422bc7561a26b1ac817b
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-14 01:02:15 +0530

    Create main.tf

---
+++ terraform/modules/vnet/main.tf
@@ -0,0 +1,57 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.project_name}-vnet"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  address_space = [var.vnet_cidr]
+
+  tags = {
+    Project = var.project_name
+  }
+}
+
+# AKS subnet
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.aks_subnet_cidr]
+}
+
+# Database subnet
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "db-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.db_subnet_cidr]
+
+  delegation {
+    name = "mysql-delegation"
+
+    service_delegation {
+      name = "Microsoft.DBforMySQL/flexibleServers"
+
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action"
+      ]
+    }
+  }
+}
+
+# Application Gateway subnet
+resource "azurerm_subnet" "appgw_subnet" {
+  name                 = "appgw-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.appgw_subnet_cidr]
+}
+
+# Private Endpoint subnet
+resource "azurerm_subnet" "private_endpoint_subnet" {
+  name                 = "private-endpoint-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.private_endpoint_subnet_cidr]
+
+  private_endpoint_network_policies_enabled = false
+}
