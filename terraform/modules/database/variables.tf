61bb71376d4647f8b9c28a9e1155a663579d0b68
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-18 00:41:52 +0530

    Update variables.tf

--- terraform/modules/database/variables.tf
+++ terraform/modules/database/variables.tf
@@ -1,29 +1,9 @@
-resource "azurerm_mysql_flexible_server" "mysql" {
-  name                   = "${var.project_name}-mysql"
-  resource_group_name    = var.resource_group_name
-  location               = var.location
+variable "project_name" {}
+variable "location" {}
+variable "resource_group_name" {}
 
-  administrator_login    = var.db_admin_username
-  administrator_password = var.db_admin_password
+variable "db_name" {}
+variable "db_admin_user" {}
+variable "db_admin_password" {}
 
-  sku_name               = var.sku_name
-  version                = "8.0.21"
-
-  delegated_subnet_id    = var.db_subnet_id
-  private_dns_zone_id    = var.private_dns_zone_id
-
-  backup_retention_days  = 7
-  geo_redundant_backup_enabled = false
-
-  public_network_access_enabled = false
-
-  tags = var.tags
-}
-
-resource "azurerm_mysql_flexible_database" "db" {
-  name                = var.db_name
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_mysql_flexible_server.mysql.name
-  charset             = "utf8"
-  collation           = "utf8_unicode_ci"
-}
+variable "db_subnet_id" {}
