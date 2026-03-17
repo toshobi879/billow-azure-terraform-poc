a19ad3d872749943215bd14b8c15421a64975fc4
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-18 00:47:05 +0530

    Update main.tf

--- terraform/database/main.tf
+++ terraform/database/main.tf
@@ -9,32 +9,19 @@ data "terraform_remote_state" "network" {
   }
 }
 
-resource "azurerm_private_dns_zone" "mysql_dns" {
-  name                = "privatelink.mysql.database.azure.com"
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
-  name                  = "${var.project_name}-dns-link"
-  private_dns_zone_name = azurerm_private_dns_zone.mysql_dns.name
-  virtual_network_id    = data.terraform_remote_state.network.outputs.vnet_id
-  resource_group_name   = var.resource_group_name
-}
-
 module "database" {
   source = "../modules/database"
 
   project_name        = var.project_name
-  resource_group_name = var.resource_group_name
   location            = var.location
+  resource_group_name = var.resource_group_name
 
-  db_admin_username   = var.db_admin_username
-  db_admin_password   = var.db_admin_password
   db_name             = var.db_name
-  sku_name            = var.sku_name
+  db_admin_user       = var.db_admin_user
+  db_admin_password   = var.db_admin_password
 
   db_subnet_id        = data.terraform_remote_state.network.outputs.db_subnet_id
-  private_dns_zone_id = azurerm_private_dns_zone.mysql_dns.id
 
-  tags = var.tags
+  sku_name            = var.sku_name
+  tags                = var.tags
 }
