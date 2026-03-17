9a3397d3c3525d649315b297c9f5a342e5c90fb4
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-18 00:46:26 +0530

    Update variables.tf

--- terraform/modules/database/variables.tf
+++ terraform/modules/database/variables.tf
@@ -3,7 +3,18 @@ variable "location" {}
 variable "resource_group_name" {}
 
 variable "db_name" {}
+
 variable "db_admin_user" {}
 variable "db_admin_password" {}
 
 variable "db_subnet_id" {}
+
+# optional but recommended
+variable "sku_name" {
+  default = "B_Standard_B1ms"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
