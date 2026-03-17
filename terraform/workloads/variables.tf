5de3dec312640d3d3ef24a25123d3080f312a419
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-17 15:23:55 +0530

    Update variables.tf

--- terraform/workloads/variables.tf
+++ terraform/workloads/variables.tf
@@ -8,6 +8,3 @@ variable "db_subnet_cidr" {}
 variable "appgw_subnet_cidr" {}
 variable "private_endpoint_subnet_cidr" {}
 
-variable "project_name" {}
-variable "location" {}
-variable "resource_group_name" {}
