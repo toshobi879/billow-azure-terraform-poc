1f286672fd9003d2d6e0d3325da3b75d96d7427f
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-18 00:46:43 +0530

    Update main.tf

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_mysql_flexible_server" "mysql" {
   administrator_login    = var.db_admin_user
   administrator_password = var.db_admin_password
 
-  sku_name               = "B_Standard_B1ms"   # cheapest
+  sku_name               = var.sku_name
   version                = "8.0.21"
 
   delegated_subnet_id    = var.db_subnet_id
@@ -15,6 +15,8 @@ resource "azurerm_mysql_flexible_server" "mysql" {
   backup_retention_days  = 7
 
   public_network_access_enabled = false
+
+  tags = var.tags
 }
 
 resource "azurerm_mysql_flexible_database" "db" {
