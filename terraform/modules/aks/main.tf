dd02c9e34f1e045959519f5cf86a837bfb7f99f1
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-17 15:15:40 +0530

    Create main.tf

---
+++ terraform/modules/aks/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.project_name}-aks"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "${var.project_name}-dns"
+
+  default_node_pool {
+    name           = "nodepool1"
+    node_count     = 1
+    vm_size        = "Standard_B2s"   # cheapest stable option
+    vnet_subnet_id = var.aks_subnet_id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  tags = {
+    Environment = "POC"
+  }
+}
