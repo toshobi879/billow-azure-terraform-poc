3dbf70d6fa276bc62b3776c8b1aface8c7ef3b29
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-17 15:16:34 +0530

    Update main.tf

--- terraform/workloads/main.tf
+++ terraform/workloads/main.tf
@@ -11,3 +11,16 @@ module "vnet" {
   appgw_subnet_cidr             = var.appgw_subnet_cidr
   private_endpoint_subnet_cidr  = var.private_endpoint_subnet_cidr
 }
+
+
+
+####
+module "aks" {
+  source = "../modules/aks"
+
+  project_name        = var.project_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  aks_subnet_id = module.vnet.aks_subnet_id
+}
