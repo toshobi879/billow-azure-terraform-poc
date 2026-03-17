ad9f11bf19fc06ffe13faa6e3527a9db06bee837
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-17 16:18:55 +0530

    Update main.tf

--- terraform/workloads/main.tf
+++ terraform/workloads/main.tf
@@ -11,27 +11,3 @@ module "vnet" {
   appgw_subnet_cidr             = var.appgw_subnet_cidr
   private_endpoint_subnet_cidr  = var.private_endpoint_subnet_cidr
 }
-
-
-
-####
-data "terraform_remote_state" "vnet" {
-  backend = "azurerm"
-
-  config = {
-    resource_group_name  = "Terraform_IaC_POC"
-    storage_account_name = "tfstatebillowpoc123"
-    container_name       = "tfstatepoc"
-    key                  = "network-dev.tfstate"
-  }
-}
-
-module "aks" {
-  source = "../modules/aks"
-
-  project_name        = var.project_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  aks_subnet_id = data.terraform_remote_state.vnet.outputs.aks_subnet_id
-}
