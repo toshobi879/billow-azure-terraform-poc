a167de5cebeab25d31475684253b281d4ecc3a6f
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-18 12:00:17 +0530

    Update outputs.tf

--- terraform/database/outputs.tf
+++ terraform/database/outputs.tf
@@ -1,7 +1,17 @@
-output "mysql_fqdn" {
-  value = module.database.mysql_fqdn
+output "db_host" {
+  value = module.database.db_host
 }
 
-output "mysql_server_name" {
-  value = module.database.mysql_server_name
+output "db_name" {
+  value = module.database.db_name
+}
+
+output "db_user" {
+  value     = module.database.db_user
+  sensitive = true
+}
+
+output "db_password" {
+  value     = module.database.db_password
+  sensitive = true
 }
