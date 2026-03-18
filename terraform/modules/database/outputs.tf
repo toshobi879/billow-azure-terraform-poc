f6c56de4f50e222cb689479caf2e573189be4c23
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-03-18 12:00:07 +0530

    Update outputs.tf

--- terraform/modules/database/outputs.tf
+++ terraform/modules/database/outputs.tf
@@ -1,7 +1,17 @@
-output "mysql_server_name" {
-  value = azurerm_mysql_flexible_server.mysql.name
+output "db_host" {
+  value = azurerm_mysql_flexible_server.mysql.fqdn
 }
 
-output "mysql_fqdn" {
-  value = azurerm_mysql_flexible_server.mysql.fqdn
+output "db_name" {
+  value = var.db_name
+}
+
+output "db_user" {
+  value     = "${var.db_admin_user}@${azurerm_mysql_flexible_server.mysql.name}"
+  sensitive = true
+}
+
+output "db_password" {
+  value     = var.db_admin_password
+  sensitive = true
 }
